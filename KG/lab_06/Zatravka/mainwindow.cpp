8745afee10225cd4020424176a22a392dc50eb77
Zeynal <zeynal.zeynalov.00@mail.ru>
2019-04-14 12:29:16 +0300

    lab06 was partly solved

--- KG/lab_06/Zatravka/mainwindow.cpp
+++ KG/lab_06/Zatravka/mainwindow.cpp
@@ -1,14 +1,419 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPainter>
+#include <QDebug>
+#include <QMouseEvent>
+#include <QColorDialog>
+#include <QMessageBox>
+#include <QPixmapCache>
+#include <iostream>
+#include <windows.h>
+#include <math.h>
+
+#define OFFSET_X_MOUSE 8
+#define OFFSET_Y_MOUSE 44
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->draw_label->setPalette(QColor(Qt::white));
+
+
+    scene = new QPixmap(881, 561);
+    scene->fill(QColor(Qt::white));
+
+    painter = new QPainter(scene);
+    painter->setPen(Qt::black);
+    ui->draw_label->setPixmap(*scene);
+
+    QPalette Pal(palette());
+    Pal.setColor(QPalette::Background, Qt::black);
+
+    ui->border_color_label->setAutoFillBackground(true);
+    ui->border_color_label->setPalette(Pal);
+
+    Pal.setColor(QPalette::Background, Qt::blue);
+    ui->filling_color_label->setAutoFillBackground(true);
+    ui->filling_color_label->setPalette(Pal);
+
+    border_color = Qt::black;
+    filling_color = Qt::blue;
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_add_point_button_clicked()
+{
+    bool ok1 = true, ok2 = false;
+    QString x_value = ui->entry_x->text();
+    QString y_value = ui->entry_y->text();
+    double x = x_value.toDouble(&ok1);
+    double y = y_value.toDouble(&ok2);
+    if (ok1 && ok2)
+    {
+        painter->setPen(QColor(border_color));
+        if (prev_x == -1 && prev_y == -1)
+        {
+            first_x = x;
+            first_y = y;
+        }
+        else
+        {
+            painter->drawLine(x, y, prev_x, prev_y);
+        }
+        painter->drawPoint(x, y);
+        prev_x = x;
+        prev_y = y;
+        ui->draw_label->setPixmap(*scene);
+    }
+    else
+    {
+        QMessageBox msgBox;
+        msgBox.setText("Подождите!");
+        // Тип иконки сообщения
+        msgBox.setIcon(QMessageBox::Information);
+        msgBox.setInformativeText("Введены неверные параметры");
+        // На какой кнопке фокусироваться по умолчанию
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    }
+
+}
+
+void MainWindow::on_close_point_button_clicked()
+{
+    if (prev_x != -1 && prev_y != -1)
+    {
+        painter->drawLine(prev_x, prev_y, first_x, first_y);
+        ui->draw_label->setPixmap(*scene);
+        prev_x = -1;
+        prev_y = -1;
+    }
+
+}
+
+void MainWindow::on_border_color_button_clicked()
+{
+    border_color = QColorDialog::getColor(Qt::black, this);
+    QPalette Pal(palette());
+    Pal.setColor(QPalette::Background, border_color);
+    ui->border_color_label->setAutoFillBackground(true);
+    ui->border_color_label->setPalette(Pal);
+    ui->border_color_label->show();
+}
+
+void MainWindow::on_filing_color_button_clicked()
+{
+    filling_color = QColorDialog::getColor(filling_color, this);
+    QPalette Pal(palette());
+    Pal.setColor(QPalette::Background, filling_color);
+    ui->filling_color_label->setAutoFillBackground(true);
+    ui->filling_color_label->setPalette(Pal);
+    ui->filling_color_label->show();
+}
+
+typedef struct POINTSS points_t;
+struct POINTSS
+{
+    double x;
+    double y;
+};
+
+class stack_c
+{
+public:
+    void push(points_t x)
+    {
+        stack_t.push_back(x);
+    }
+    points_t pop()
+    {
+        points_t x = stack_t.value(stack_t.size() - 1);
+        stack_t.pop_back();
+        return x;
+    }
+    bool is_empty()
+    {
+        if (stack_t.isEmpty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+private:
+    QVector <points_t> stack_t;
+};
+
+void MainWindow::on_fill_button_clicked()
+{
+    stack_c stack;
+    points_t point;
+    point.x = pixel_begin_x;
+    point.y = pixel_begin_y;
+    stack.push(point);
+    // if pixel was set
+    if (flag_pixel_set)
+    {
+        // drawing with algorithm
+        while (!stack.is_empty())
+        {
+
+            point = stack.pop();
+            double x = point.x;
+            double y = point.y;
+            double x_temp = x;
+            int flag = 0;
+            painter->setPen(QColor(filling_color));
+            painter->drawPoint(x, y);
+            x--;
+            img = scene->toImage();
+            QColor color = img.pixelColor(x, y);
+            while (color != border_color)
+            {
+                color = img.pixelColor(x, y);
+                qDebug() << "1";
+                painter->drawPoint(x, y);
+                x--;
+            }
+            double xl = x + 1;
+            x = x_temp;
+            x++;
+            qDebug() << "2";
+            color = img.pixelColor(x, y);
+            while (color != border_color)
+            {
+                color = img.pixelColor(x, y);
+                painter->drawPoint(x, y);
+                x++;
+            }
+            double xr = x - 1;
+            y = y + 1;
+            x = xl;
+            while (x <= xr)
+            {
+                flag = 0;
+                color = img.pixelColor(x, y);
+                while (color != border_color && color != filling_color && x <= xr)
+                {
+                    color = img.pixelColor(x, y);
+                    if (flag == 0)
+                        flag = 1;
+                    x++;
+                }
+
+                if (flag == 1)
+                {
+                    if (x == xr && color != filling_color && color != border_color)
+                    {
+                        point.x = x;
+                        point.y = y;
+                        stack.push(point);
+                    }
+                    else
+                    {
+                        point.x = x - 1;
+                        point.y = y;
+                        stack.push(point);
+                    }
+                    flag = 0;
+                }
+                x_temp = x;
+                while (color != border_color && color != filling_color && x < xr)
+                {
+                    color = img.pixelColor(x, y);
+                    x++;
+                }
+
+                if (x == x_temp)
+                {
+                    x++;
+                }
+            }
+            y = y - 2;
+            x = xl;
+            while (x <= xr)
+            {
+                flag = 0;
+                color = img.pixelColor(x, y);
+                while (color != border_color && color != filling_color && x <= xr)
+                {
+                    color = img.pixelColor(x, y);
+                    if (flag == 0)
+                        flag = 1;
+                    x++;
+                }
+
+                if (flag == 1)
+                {
+                    if (x == xr && color != filling_color && color != border_color)
+                    {
+                        point.x = x;
+                        point.y = y;
+                        stack.push(point);
+                    }
+                    else
+                    {
+                        point.x = x - 1;
+                        point.y = y;
+                        stack.push(point);
+                    }
+                    flag = 0;
+                }
+                x_temp = x;
+                while (color != border_color && color != filling_color && x < xr)
+                {
+                    color = img.pixelColor(x, y);
+                    x++;
+                }
+
+                if (x == x_temp)
+                {
+                    x++;
+                }
+            }
+
+        }
+        ui->draw_label->setPixmap(*scene);
+
+    }
+    else
+    {
+        QMessageBox msgBox;
+        msgBox.setText("Подождите!");
+        // Тип иконки сообщения
+        msgBox.setIcon(QMessageBox::Information);
+        msgBox.setInformativeText("Затравочный пиксель не установлен");
+        // На какой кнопке фокусироваться по умолчанию
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    }
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    painter->setPen(QPen(border_color));
+    double x = event->x();
+    double y = event->y();
+    x -= OFFSET_X_MOUSE;
+    y -= OFFSET_Y_MOUSE;
+    // where to draw // right border x0 + 10  y0 + 40 (Offset from begining of window)
+    if (ui->polygon_radio->isChecked())
+    {
+        if (x >= 0 && y >= 0 && x <= 880 && y <= 561)
+        {
+            if (flag_first_touched == false)
+            {
+                first_x = x;
+                first_y = y;
+                flag_first_touched = true;
+            }
+            if (event->button() == Qt::LeftButton)
+            {
+                if (event->modifiers() == Qt::ShiftModifier)
+                {
+                    if (fabs(prev_x - x) < fabs(prev_y - prev_x))
+                    {
+                        x = prev_x;
+                    }
+                    else
+                    {
+                        y = prev_y;
+                    }
+                }
+                painter->drawPoint(x, y);
+                if (prev_x != -1 && prev_y != -1)
+                {
+                    painter->drawLine(prev_x, prev_y, x, y);
+                }
+                prev_x = x;
+                prev_y = y;
+            }
+            else if (event->button() == Qt::RightButton)
+            {
+                if (prev_x != -1 && prev_y != -1)
+                {
+                    painter->drawLine(prev_x, prev_y, first_x, first_y);
+                    prev_x = -1;
+                    prev_y = -1;
+                    flag_first_touched = false;
+                }
+            }
+        }
+        ui->draw_label->setPixmap(*scene);
+    }
+    else if (ui->pixel_radio->isChecked())
+    {
+        pixel_begin_x = x;
+        pixel_begin_y = y;
+        flag_pixel_set = true;
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (ui->polygon1_radio->isChecked())
+    {
+        painter->setPen(QPen(border_color));
+        double x = event->x();
+        double y = event->y();
+        x -= OFFSET_X_MOUSE;
+        y -= OFFSET_Y_MOUSE;
+        if (x >= 0 && y >= 0 && x <= 880 && y <= 561)
+        {
+            if (event->modifiers() == Qt::ShiftModifier)
+            {
+                if (prev_x != -1 && prev_y != -1)
+                {
+                    painter->drawLine(prev_x, prev_y, first_x, first_y);
+                    prev_x = -1;
+                    prev_y = -1;
+                    flag_first_touched = false;
+                }
+            }
+            else
+            {
+                painter->drawPoint(x, y);
+                if (prev_x != -1 && prev_y != -1)
+                {
+                    painter->drawLine(prev_x, prev_y, x, y);
+                }
+                else
+                {
+                    first_x = x;
+                    first_y = y;
+                }
+                prev_x = x;
+                prev_y = y;
+            }
+            ui->draw_label->setPixmap(*scene);
+        }
+    }
+
+}
+
+void MainWindow::on_clear_button_clicked()
+{
+    delete painter;
+    delete scene;
+    edges.clear();
+    ui->draw_label->clear();
+    scene = new QPixmap(881, 561);
+    scene->fill(QColor(Qt::white));
+    painter = new QPainter(scene);
+    ui->draw_label->setPixmap(*scene);
+    prev_x = -1;
+    prev_y = -1;
+    flag_first_attained = false;
+    flag_first_touched = false;
 }
--- KG/lab_06/Zatravka/mainwindow.h
+++ KG/lab_06/Zatravka/mainwindow.h
@@ -2,6 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPixmap>
+
+typedef struct EDGE edge_t;
+struct EDGE
+{
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+};
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +24,40 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    // -1 means start status
+    double prev_x = -1;
+    double prev_y = -1;
+    double pixel_begin_x = -1;
+    double pixel_begin_y = -1;
+    double first_x;
+    double first_y;
+    bool flag_first_touched = false;
+    bool flag_first_attained = false;
+    bool flag_pixel_set = false; // for filling may be only 1
+private slots:
+    void on_add_point_button_clicked();
+
+    void on_close_point_button_clicked();
+
+    void on_border_color_button_clicked();
+
+    void on_filing_color_button_clicked();
+
+    void on_fill_button_clicked();
+
+    void on_clear_button_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QPixmap *scene;
+    QColor border_color;
+    QColor filling_color;
+    QPainter *painter;
+    QImage img;
+    QVector <edge_t> edges;
+
 };
 
 #endif // MAINWINDOW_H
