93e088989201fc11724a1b375a2b9ad107da0fb3
Zeynal <zeynal.zeynalov.00@mail.ru>
2019-05-05 21:42:59 +0300

    lab_06 was added

--- Algorithms/lab06_diff/untitled/main.cpp
+++ Algorithms/lab06_diff/untitled/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #define OK 0
 #define ERR_ALLOCATION -1
@@ -56,30 +57,96 @@ int CreateTable(table_t &table, int begin, int end, int step)
 
 void print_table(table_t table)
 {
+    printf("|------------------------------------------------------------------------|\n");
+    printf("|   x       y      y'(x)    unknown    Mid     Runge  Alignment  Pure    |\n");
+    printf("|------------------------------------------------------------------------|\n");
     for (unsigned int i = 0; i < table.rowCount; i++)
     {
         for (unsigned int j = 0; j < table.columnCount; j++)
         {
-            printf("%-7.4f ", table.table[i][j]);
+            if (j == 0)
+            {
+                printf("|");
+            }
+
+            printf("%-8.4f ", table.table[i][j]);
+            if (j == table.columnCount - 1)
+            {
+                printf("|");
+            }
         }
         cout << endl;
+        printf("|________________________________________________________________________|\n");
     }
 }
 
-double first(double y, double yNext, double h)
+
+
+double rightDiff(double y, double yNext, double h)
 {
+    // Правая разность
     return (yNext - y) / h;
 }
 
-void fillTable(table_t &table, int h)
+double midDdiff(double yPrev, double yNext, double h)
 {
+    return (yNext - yPrev) / (2.0 * h);
+}
+
+double RungeDiff(double yNext, double yPREV, double yNr, double yPr, double r, double h)
+{
+        double y1 = (yNext - yPREV) / ( 2 * h);
+        double y2 = (yNr - yPr) / (2 * h * r);
+        return y1 + (y1 - y2) / (pow(r, 2) - 1);
+}
+
+double func(int *a, double x)
+{
+    return (a[0] * x) / (a[1] + a[2] * x);
+}
+double getH(double x1, double x2)
+{
+    return fabs(x1 - x2);
+}
+double getAlignment(double x, double y, double rightX, double rightY)
+{
+    return -((y*y) / (x*x)) * rightDiff(f(rightY), f(y), getH(f(x), f(rightX)));
+}
+void fillTable(table_t &table, int h, unsigned int r)
+{
+    int a[3] = {1, 2, 3};
     for (unsigned int i = 0; i < table.rowCount; i++)
     {
+        // 1
         if (i != 0)
         {
-            table.table[i][2] = first(table.table[i - 1][1], table.table[i][1], h);
+            table.table[i][2] = rightDiff(table.table[i - 1][1], table.table[i][1], h);
         }
 
+        // 2
+
+        // 3 Центральная разность
+        if (i != 0 && i != table.rowCount - 1)
+        {
+            table.table[i][4] = midDdiff(table.table[i - 1][1], table.table[i + 1][1], h);
+        }
+        // 4. 2-я формула Рунге
+        if (i > r - 1 && i < table.rowCount - r)
+        {
+            table.table[i][5] = RungeDiff(table.table[i + 1][1], table.table[i - 1][1],
+                    table.table[i + r][1], table.table[i - r][1], r, h);
+        }
+        // 5
+        //table.table[i][6] = func(a, table.table[i][0]);
+
+        if (i == table.rowCount - 1)
+        {
+            table.table[i][6] = -getAlignment(table.table[i][0], table.table[i][1], table.table[i - 1][0], table.table[i - 1][1]);
+        }
+        else
+        {
+            table.table[i][6] = getAlignment(table.table[i][0], table.table[i][1], table.table[i + 1][0], table.table[i + 1][1]);
+        }
     }
 }
 
@@ -92,7 +159,7 @@ int main()
     int code_error = CreateTable(table, begin_x, end_x, step);
     if (code_error == OK)
     {
-        fillTable(table, step);
+        fillTable(table, step, 2);
         print_table(table);
     }
 
