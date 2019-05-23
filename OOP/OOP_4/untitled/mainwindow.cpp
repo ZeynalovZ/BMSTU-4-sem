b282e91a0cef8993d834acac9ea18354610ece75
Zeynal <zeynal.zeynalov.00@mail.ru>
2019-05-23 23:45:17 +0300

    strategy pattern was added

--- OOP/OOP_4/untitled/mainwindow.cpp
+++ OOP/OOP_4/untitled/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QDebug>
+#include <QObject>
+void use();
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    use();
 }
 
 MainWindow::~MainWindow()
@@ -13,7 +16,55 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-class Vehicle
+class strategy
 {
+public:
 
+    virtual void execute(int a, int b) = 0;
 };
+
+class ConcreteStrategyMultiply : public strategy
+{
+public:
+    void execute(int a, int b)
+    {
+        qDebug() << "a * b = " << a * b;
+
+    }
+};
+
+class ConcreteStrategySum : public strategy
+{
+public:
+    void execute(int a, int b)
+    {
+        qDebug() << "a + b = " << a + b;
+    }
+};
+
+class Context
+{
+public:
+
+    void SetStrategy(strategy *str_)
+    {
+        this->str = str_;
+    }
+    void executeStrategy(int a, int b)
+    {
+        this->str->execute(a, b);
+    }
+private:
+    strategy *str;
+};
+
+void use()
+{
+    Context cntxt;
+    cntxt.SetStrategy(new ConcreteStrategyMultiply);
+    cntxt.executeStrategy(1, 1);
+
+    cntxt.SetStrategy(new ConcreteStrategySum);
+    cntxt.executeStrategy(11, 2);
+
+}
